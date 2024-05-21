3d2b3f7d4404b6fc34cc639cfb95a6b38dd1dee9
will-kyaria <will@kyaria.ai>
2024-05-21 15:22:42 -0700

    fixed barchart

--- app/components/barchart.tsx
+++ app/components/barchart.tsx
@@ -12,7 +12,7 @@ interface BarChartProps<T> {
 
 const BarChartComponent = <T extends object>({ data, dataKeyX, dataKeyY, title }: BarChartProps<T>) => {
     return (
-        <div className='bg-white shadow-md rounded-lg flex flex-col p-4'>
+        <div className='bg-white shadow-md rounded-lg flex flex-col p-4 h-96'>
             <h1 className="text-xl font-semibold mb-4 text-center">{title}</h1>
             <ResponsiveContainer width="100%" height="100%">
                 <BarChart data={data}>

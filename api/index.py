50bf59047632127344c7cf500d1dff07423c61ee
will-kyaria <will@kyaria.ai>
2024-05-21 18:38:18 -0700

    switched to asyncio

--- api/index.py
+++ api/index.py
@@ -255,5 +255,4 @@ async def get_trials_by_condition():
 
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    asyncio.run(app, host="0.0.0.0", port=8000)
